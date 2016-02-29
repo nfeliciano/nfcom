29368016d9d559acee5ce800a0d619daf114edd1
nfeliciano <feliciano.noel@gmail.com>
2016-02-29 04:36:48 -0800

    bypass bin/www

--- app.js
+++ app.js
@@ -35,6 +35,14 @@ app.use(function(req, res, next) {
   next(err);
 });
 
+var http = require('http')
+var port = process.env.PORT || 1337;
+http.createServer(function(req, res) {
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  res.end('Hello World\n');
+}).listen(port);
+
+
 // error handlers
 
 // development error handler

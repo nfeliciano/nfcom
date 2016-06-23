616238a11591e01ebafd7ab4d44a3246f2d0dd4e
nfeliciano <feliciano.noel@gmail.com>
2016-06-22 20:18:58 -0700

    starting auth

---
+++ backend/auth/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+from django.contrib.auth import login, logout
+
+# Create your views here.
+class AuthView(APIView):
+    authentication_classes = (QuietBasicAuthentication,)
+
+    def post(self, request, *args, **kwargs):
+        login(request, request.user)
+        return Response(UserSerializer(request.user).data)
+
+    def delete(self, request, *args, **kwargs):
+        logout(request)
+        return Response({})
